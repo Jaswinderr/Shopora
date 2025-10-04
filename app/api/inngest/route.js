5f2a7ffc9d326a4599a188832fd362c3fc897b46
Jaswinder Singh <jaswindere9@gmail.com>
2025-10-04 18:04:43 -0400

    fixed route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,20 +1,11 @@
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 import { serve } from "inngest/next";
 
-export const GET = async (req, res) => {
-  res.status(200).json({ message: "Inngest GET endpoint" });
-};
-
-export const POST = async (req, res) => {
-  const { inngest, syncUserCreation, syncUserUpdation, syncUserDeletion } = await import("@/config/inngest");
-
-  const { GET, POST, PUT } = serve({
+export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [
-      syncUserCreation,
-      syncUserUpdation,
-      syncUserDeletion,
-    ],
-  });
-
-  return POST(req, res);
-};
+        syncUserCreation,
+        syncUserUpdation,
+        syncUserDeletion
+    ]
+})
